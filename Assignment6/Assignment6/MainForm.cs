8905ba6aa9d963c864e9f8e05e979c306fcd4439
christopherlindberg1 <christopher.lindberg1@gmail.com>
2019-11-29 23:16:07 +0100

    Moved error methods to MainForm

--- Assignment6/Assignment6/MainForm.cs
+++ Assignment6/Assignment6/MainForm.cs
@@ -13,6 +13,24 @@ namespace Assignment6
     public partial class MainForm : Form
     {
 
+        private readonly TaskManager _taskManager = new TaskManager();
+        private readonly List<string> _errors = new List<string>();
+
+
+        // ===================== Properties ===================== //
+
+        private TaskManager TaskManager
+        {
+            get { return this._taskManager; }
+        }
+
+        private List<string> Errors
+        {
+            get { return this._errors; }
+        }
+
+
+
 
         // ======================= Methods ======================= //
 
@@ -31,7 +49,7 @@ namespace Assignment6
             this.dateTimePicker1.CustomFormat = " yyyy/MM/dd - hh:mm:ss";
             this.listBoxToDos.HorizontalScrollbar = true;
             this.SetToolTips();
-            
+
             this.SetFormToDefaultState();
         }
 
@@ -61,32 +79,103 @@ namespace Assignment6
 
         private bool ValidateInput()
         {
-            return false;
+            bool dateTimeOk = this.ValidateDateTime();
+            bool toDoOk = this.ValidateToDoText();
+            bool priorityOk = this.ValidatePriority();
+
+            return dateTimeOk && toDoOk && priorityOk;
         }
 
         private bool ValidateDateTime()
         {
-            return false;
+            if (this.dateTimePicker1.Value <= DateTime.Now)
+            {
+                this.AddErrorMessage("The date and time cannot be in the past");
+                return false;
+            }
+
+            return true;
         }
 
         private bool ValidateToDoText()
         {
-            return false;
+            if (String.IsNullOrWhiteSpace(this.textBoxToDo.Text.Trim()))
+            {
+                this.AddErrorMessage("You must provide a description of the task");
+                return false;
+            }
+
+            return true;
         }
 
         private bool ValidatePriority()
         {
-            return false;
+            if (this.comboBoxPriority.SelectedIndex == -1)
+            {
+                this.AddErrorMessage("You must select a priority");
+                return false;
+            }
+
+            if (!Enum.IsDefined(typeof(PriorityLevels), this.comboBoxPriority.SelectedItem))
+            {
+                this.AddErrorMessage("You must select a priority in the list");
+                return false;
+            }
+
+            return true;
         }
+
+
+
+
+        // ============== Methods for error handling ============== //
+
+        private void AddErrorMessage(string error)
+        {
+            if (String.IsNullOrWhiteSpace(error))
+                throw new ArgumentNullException("error", "error cannot be empty");
+
+            this.Errors.Add(error);
+        }
+
+        private void ShowErrorMessages()
+        {
+            StringBuilder errorMessage = new StringBuilder();
+
+            foreach (string error in this.Errors)
+            {
+                errorMessage.Append("* ");
+                errorMessage.Append(error);
+                errorMessage.Append("\n");
+            }
+
+            MessageBox.Show(errorMessage.ToString());
+
+            this.ClearErrorList();
+        }
+
+        private void ClearErrorList()
+        {
+            this.Errors.Clear();
+        }
+
 
 
 
 
         // ======================= Events ======================= //
-
         private void btnAdd_Click(object sender, EventArgs e)
         {
+            if (this.ValidateInput())
+            {
 
+
+            }
+            else
+            {
+                this.ShowErrorMessages();
+
+            }
         }
     }
 }
